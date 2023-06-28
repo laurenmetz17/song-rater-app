4ab46af386dac0f6889947de81e41b118927148f
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-06-28 17:29:49 -0400

    add basic login field

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,8 +1,16 @@
 import {React, useState, useEffect} from 'react';
+
 function Login() {
     return(
         <div>
             <h1>Login Here</h1>
+            <form id="login">
+                <h3>Username:</h3>
+                <input type="text" name="username"></input>
+                <h3>Password:</h3>
+                <input type="text" name="password"></input>
+                <input type="submit" value="Login"></input>      
+            </form>
         </div>
     )
 
