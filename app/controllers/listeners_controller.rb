84750eb8ad2f506fe758b4369d7a861b107536aa
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-06-28 13:42:44 -0400

    add basic routes and actions to listeners and songs

--- app/controllers/listeners_controller.rb
+++ app/controllers/listeners_controller.rb
@@ -3,4 +3,19 @@ class ListenersController < ApplicationController
         listeners = Listener.all 
         render json: listeners 
     end
+
+    def show 
+        listener = Listener.find(params[:id])
+        render json: listener, indclude: :ratings
+    end
+
+    def destroy 
+        #add dependent destroy
+        listener = Listener.find(params[:id])
+        listner.destroy
+        head :no_content
+    end
+    
+    def create
+    end
 end
