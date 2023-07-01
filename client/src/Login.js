3f0197680b786c8ebbc043691a154d91d8423479
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-07-01 16:36:27 -0400

    add login and signup form skeleton

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,16 +1,44 @@
 import {React, useState, useEffect} from 'react';
 
 function Login() {
+
+    const [loginForm, setLoginForm] = useState({
+        username: "",
+        password: ""
+    })
+
+    const [signupForm, setSignupForm] = useState({
+        name: "",
+        profile_pic: "",
+        username: "",
+        password: "",
+        password_confirmation: ""
+    })
+
+
     return(
         <div>
-            <h1>Login Here</h1>
+            <h1>Login</h1>
             <form id="login">
-                <h3>Username:</h3>
+                <label>Username: </label>
                 <input type="text" name="username"></input>
-                <h3>Password:</h3>
+                <label>   Password: </label>
                 <input type="text" name="password"></input>
                 <input type="submit" value="Login"></input>      
             </form>
+            <h1>Sign Up!</h1>
+            <form id="sign up">
+                <label>Name: </label>
+                <input type="text" name="name"></input>
+                <label>  Profile Picture: </label>
+                <input type="text" name="profile-pic"></input>
+                <label>Username: </label>
+                <input type="text" name="username"></input>
+                <label>Password:</label>
+                <input type="text" name="password"></input>
+                <label>Confirm Password:</label>
+                <input type="text" name="password-confirmation"></input>
+            </form>   
         </div>
     )
 
