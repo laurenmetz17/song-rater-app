b2497b66d1482ed80a3d642550faf017cd6806c7
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-07-01 16:53:53 -0400

    update form state for login and signup

--- client/src/Login.js
+++ client/src/Login.js
@@ -15,29 +15,52 @@ function Login() {
         password_confirmation: ""
     })
 
+    function handleLogin(e) {
+        e.preventDefault()
+        //write this funciton in usercontext file 
+        console.log(loginForm)
+    }
+
+    function createListener() {
+        //write this function in usercontext file
+    }
+
+    function updateLogin(e) {
+        const target = e.target.name
+        setLoginForm({...loginForm, [target] : e.target.value})
+
+    }
+
+    function updateSignup(e) {
+        const target = e.target.name
+        setSignupForm({...signupForm, [target] : e.target.value})
+        console.log(signupForm)
+    }
+
 
     return(
         <div>
             <h1>Login</h1>
-            <form id="login">
+            <form id="login" onSubmit={handleLogin}>
                 <label>Username: </label>
-                <input type="text" name="username"></input>
+                <input type="text" name="username" onChange={updateLogin}></input>
                 <label>   Password: </label>
-                <input type="text" name="password"></input>
+                <input type="text" name="password" onChange={updateLogin}></input>
                 <input type="submit" value="Login"></input>      
             </form>
             <h1>Sign Up!</h1>
-            <form id="sign up">
+            <form id="sign up" onSubmit={createListener}>
                 <label>Name: </label>
-                <input type="text" name="name"></input>
+                <input type="text" name="name" onChange={updateSignup}></input>
                 <label>  Profile Picture: </label>
-                <input type="text" name="profile-pic"></input>
+                <input type="text" name="profile_pic" onChange={updateSignup}></input>
                 <label>Username: </label>
-                <input type="text" name="username"></input>
+                <input type="text" name="username" onChange={updateSignup}></input>
                 <label>Password:</label>
-                <input type="text" name="password"></input>
+                <input type="text" name="password" onChange={updateSignup}></input>
                 <label>Confirm Password:</label>
-                <input type="text" name="password-confirmation"></input>
+                <input type="text" name="password-_confirmation" onChange={updateSignup}></input>
+                <input type="submit" value="Signup"></input>
             </form>   
         </div>
     )
