760c3dccd2e2ed8fe7854e384388ac2c4dc0ceba
laurenmetz17 <103585965+laurenmetz17@users.noreply.github.com>
2023-07-09 19:27:24 -0400

    add home about and instructions

--- client/src/Home.js
+++ client/src/Home.js
@@ -2,7 +2,20 @@
 function Home() {
 
     return (
-        <h1>Home/About</h1>
+        <div className="home">
+            <h3>Song Rater About :</h3>
+            <p>Song rater is an application that can be used to rate and comment on some of your favorite songs. Leave ratings and see other listeners ratings, as well as songs average listener rating! </p>
+            <h4> Getting started : </h4>
+            <ul>
+                <li>Login or Sign up in the top right corner</li>
+                <li>Once logged in you can browse songs you've rated on the Listener Songs tab</li>
+                <li>Browse all songs and their ratings on the Songs tab</li>
+                <li>Under each song listeners can create, delete, or edit Ratings</li>
+                <li>Listeners can only edit or delete their own ratings, and each listener can only rate a song once</li>
+                <li>When done browsing, go to the logout tab and click Logout</li>
+                <li>To return to the home page click the Song Rater icon on the top left</li>
+            </ul>
+        </div>
     )
 }
 
